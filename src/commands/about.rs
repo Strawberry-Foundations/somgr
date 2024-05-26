3ea12cf3d7b74cbbfa36212afcb7c186463e9884
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-26 20:38:51 +0200

    impl: Added update command

--- src/commands/about.rs
+++ src/commands/about.rs
@@ -3,9 +3,9 @@ use crate::statics::VERSION;
 
 pub fn about() {
     println!("\
-{BOLD}{CYAN}{UNDERLINE}StrawberryOS Manager (somgr) v{VERSION}{C_RESET}\n\
+{BOLD}{CYAN}{UNDERLINE}StrawberryOS Manager (somgr) v{}{C_RESET}\n\
 {BOLD}somgr is the management tool for StrawberryOS to configure or update your operating system. {C_RESET}
 
-");
+", *VERSION);
     std::process::exit(0);
 }
