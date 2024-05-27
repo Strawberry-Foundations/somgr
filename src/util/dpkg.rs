bfae19630c4e4df86221a8373a08a15324aaec6e
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-27 18:39:33 +0200

    impl: Optimized code duplicate

---
+++ src/util/dpkg.rs
@@ -0,0 +1,22 @@
+use std::fs::File;
+use std::io::BufRead;
+
+pub fn get_packages() -> Vec<String> {
+    subprocess::Exec::shell(
+        "/usr/sbin/chroot / dpkg --get-selections | grep -v deinstall | awk '{print $1}' > /var/cache/apt/package_list.txt"
+    ).popen().unwrap();
+
+    let file = File::open("/var/cache/apt/package_list.txt").unwrap();
+    let reader = std::io::BufReader::new(file);
+
+    let mut packages = Vec::new();
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        for word in line.split_whitespace() {
+            packages.push(word.to_string());
+        }
+    }
+    
+    packages
+}
