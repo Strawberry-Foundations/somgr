5bdc2c5289c6848ab1ce5a003925f833c57a0d9e
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-12 21:49:35 +0200

    impl: added fs mounting of relevant file systems

--- src/commands/shell.rs
+++ src/commands/shell.rs
@@ -1,7 +1,18 @@
-use stblib::colors::{BOLD, C_RESET, GREEN, RESET};
+use crate::log_info;
 
 pub fn shell() {
-    println!("{BOLD}{GREEN}=>{RESET} Entering chroot ...{C_RESET}");
+    log_info!("Mounting file systems ...");
+    subprocess::Exec::shell("sh -c 'mount --bind /dev /system/dev'").popen().unwrap();
+    subprocess::Exec::shell("sh -c 'mount --bind /sys /system/sys'").popen().unwrap();
+    subprocess::Exec::shell("sh -c 'mount --bind /proc /system/proc'").popen().unwrap();
+    
+    log_info!("Entering chroot ...");
     subprocess::Exec::shell("sh -c '/usr/sbin/chroot /system'").popen().unwrap();
-    println!("{BOLD}{GREEN}=>{RESET} Leaving chroot ...{C_RESET}");
+
+    log_info!("Unmounting file systems ...");
+    subprocess::Exec::shell("sh -c 'umount /system/dev'").popen().unwrap();
+    subprocess::Exec::shell("sh -c 'umount /system/sys'").popen().unwrap();
+    subprocess::Exec::shell("sh -c 'umount /system/proc'").popen().unwrap();
+    
+    log_info!("Leaving chroot ...");
 }
