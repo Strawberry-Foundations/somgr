135aadc4ba4827858425f930d24b4613f9db8d46
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-12 21:52:59 +0200

    impl: renamed remount.rs to mount.rs

--- src/commands/mount.rs
+++ src/commands/mount.rs
@@ -4,3 +4,10 @@ pub fn remount() {
     println!("{BOLD}{GREEN}=>{RESET} Remounting /system in rw mode...{C_RESET}");
     subprocess::Exec::shell("mount -o remount,rw /system").popen().unwrap();
 }
+
+pub fn mount() {
+    println!("{BOLD}{GREEN}=>{RESET} Binding /dev, /sys and /proc to /system ...{C_RESET}");
+    subprocess::Exec::shell("mount -o remount,rw /system").popen().unwrap();
+}
+
+
