4cf2d5de5b9c77241f7ed20259c8674994c1f611
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-15 16:45:17 +0200

    fmt: Fixed formatting

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -10,23 +10,23 @@ pub fn update() {
         log_fail!("somgr can only be executed on a StrawberryOS system");
         std::process::exit(1);
     }
-    
+
     log_info!("Starting os update ...");
-    
+
     remount(&MountType::ReadWrite);
     mount();
-    
+
     if OPTIONS.yes {
-        subprocess::Exec::shell("/usr/sbin/chroot /system sh -c 'apt update && apt -y upgrade'").popen().unwrap();    
+        subprocess::Exec::shell("/usr/sbin/chroot /system sh -c 'apt update && apt -y upgrade'").popen().unwrap();
     }
     else {
         subprocess::Exec::shell("/usr/sbin/chroot /system sh -c 'apt update && apt upgrade'").popen().unwrap();
     }
-    
+
 
     lock();
     remount(&MountType::ReadOnly);
     umount();
-    
+
     log_info!("Finished os update ...");
 }
