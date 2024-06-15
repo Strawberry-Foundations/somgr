300c45c363d16ed691a4e2c895a19b6b1619262a
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-15 16:02:25 +0200

    impl: Added rw/ ro option for remount command

--- src/commands/mount.rs
+++ src/commands/mount.rs
@@ -1,9 +1,22 @@
 use crate::log_info;
 use crate::util::fs::{mount_system, umount_system};
 
-pub fn remount() {
-    log_info!("Remounting /system in rw mode...");
-    subprocess::Exec::shell("mount -o remount,rw /system").popen().unwrap();
+pub enum MountType {
+    ReadWrite,
+    ReadOnly
+}
+
+pub fn remount(mount_type: MountType) {
+    match mount_type {
+        MountType::ReadWrite => {
+            log_info!("Remounting /system in rw mode...");
+            subprocess::Exec::shell("mount -o remount,rw /system").popen().unwrap();
+        }
+        MountType::ReadOnly => {
+            log_info!("Remounting /system in ro mode...");
+            subprocess::Exec::shell("mount -o remount,ro /system").popen().unwrap();
+        }
+    }
 }
 
 pub fn mount() {
