ad32b7e554d0ad38744dfb357195da581920410f
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-15 16:57:52 +0200

    impl: Added root_verifier()

--- src/util/verification.rs
+++ src/util/verification.rs
@@ -1,9 +1,17 @@
 use std::path::Path;
 use crate::log_fail;
+use crate::util::permissions::check_root_permissions;
 
 pub fn os_verifier() {
     if !Path::new("/user").exists() {
         log_fail!("somgr can only be executed on a StrawberryOS system");
+        std::process::exit(1);
+    }
+}
+
+pub fn root_verifier() {
+    if !check_root_permissions() {
+        log_fail!("You need to be root to use this command");
         std::process::exit(1);
     }
 }
