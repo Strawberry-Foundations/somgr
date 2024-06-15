24dae8ccfa84b4ba53c9f99d3f7f23015ab24c46
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-15 16:30:19 +0200

    impl: Added log_error

--- src/util/log.rs
+++ src/util/log.rs
@@ -4,3 +4,10 @@ macro_rules! log_info {
         println!("\x1b[1m\x1b[42m   INFO   \x1b[0m  {}\x1b[0m", $string);
     };
 }
+
+#[macro_export]
+macro_rules! log_error {
+    ($string:expr) => {
+        println!("\x1b[1m\x1b[41m   FAIL   \x1b[0m  {}\x1b[0m", $string);
+    };
+}
