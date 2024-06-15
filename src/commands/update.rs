5343a947c2393326e330fdaca1ab9b90b7c0dc3e
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-15 16:58:41 +0200

    impl: Added root_verifier()

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -1,12 +1,13 @@
 use crate::commands::lock::lock;
 use crate::commands::mount::{mount, umount, remount, MountType};
 use crate::args::OPTIONS;
-use crate::util::verification::os_verifier;
+use crate::util::verification::{os_verifier, root_verifier};
 use crate::log_info;
 
 pub fn update() {
     os_verifier();
-    
+    root_verifier();
+
     log_info!("Starting os update ...");
 
     remount(&MountType::ReadWrite);
