dec717bc2b6e03c39a94e8bddadbed3bd788734e
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-15 16:02:56 +0200

    impl: update will now remount system partition

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -1,15 +1,16 @@
 use crate::commands::lock::lock;
-use crate::commands::mount::{mount, umount, remount};
+use crate::commands::mount::{mount, umount, remount, MountType};
 use crate::log_info;
 
 pub fn update() {
     log_info!("Starting os update ...");
     
-    remount();
+    remount(MountType::ReadWrite);
     mount();
     subprocess::Exec::shell("/usr/sbin/chroot /system apt update && apt -q upgrade").popen().unwrap();
 
     lock();
+    remount(MountType::ReadOnly);
     umount();
     
     log_info!("Finished os update ...");
