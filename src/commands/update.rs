55fb6b595ccb8b9934b11b000268c8c13bcc0d3b
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-15 16:30:55 +0200

    impl: Added os check

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -1,8 +1,14 @@
+use std::path::Path;
 use crate::commands::lock::lock;
 use crate::commands::mount::{mount, umount, remount, MountType};
-use crate::log_info;
+use crate::{log_fail, log_info};
 
 pub fn update() {
+    if !Path::new("/user").exists() {
+        log_fail!("somgr can only be executed on a StrawberryOS system");
+        std::process::exit(1);
+    }
+    
     log_info!("Starting os update ...");
     
     remount(&MountType::ReadWrite);
