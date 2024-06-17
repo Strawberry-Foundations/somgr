b2075842f493e6b5d07929f45e5580855ea199c2
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-17 21:43:32 +0200

    impl: added backup status

--- src/commands/backup.rs
+++ src/commands/backup.rs
@@ -1,8 +1,11 @@
-use stblib::colors::{BOLD, C_RESET, RED};
+use indicatif::{ProgressBar, ProgressStyle};
+use stblib::colors::{BOLD, C_RESET, GREEN, RED, UNDERLINE, YELLOW};
 
 use crate::args::ARGS;
 use crate::commands;
 use crate::commands::login::Credentials;
+use crate::statics::STRAWBERRY_CLOUD_API;
+use crate::utilities::{calc_percent, format_size, serializer};
 
 pub async fn main() {
     let credentials = match Credentials::read() {
@@ -12,7 +15,7 @@ pub async fn main() {
             return;
         }
     };
-    
+
     match ARGS.subcommand.as_str() {
         "restore" => {
 
@@ -29,13 +32,59 @@ pub async fn main() {
         "list" => {
 
         }
-        "status" => {
-
-        }
+        "status" => status(credentials).await,
         _ => commands::help::help()
     }
 }
 
+#[derive(Default)]
+struct Status {
+    storage_quota_max: u64,
+    storage_quota_used: u64,
+    storage_quota_left: i64,
+}
+
 pub async fn status(credentials: Credentials) {
-    
+    let url = format!("{STRAWBERRY_CLOUD_API}/status/{}@{}", credentials.username, credentials.token);
+    let response = reqwest::get(url).await.unwrap();
+    let body = response.text().await.unwrap();
+
+    let status = if let Ok(data) = serializer(body.as_str()) {
+        if data["data"] != "Invalid credentials" && data["data"] != "Invalid username" {
+            Status {
+                storage_quota_max: data["data"]["storage_quota"]["max"].to_string().parse().unwrap(),
+                storage_quota_used: data["data"]["storage_quota"]["used"].to_string().parse().unwrap(),
+                storage_quota_left: data["data"]["storage_quota"]["left"].to_string().parse().unwrap(),
+            }
+        }
+        else {
+            eprintln!("{RED}{BOLD}Invalid credentials{C_RESET}");
+            std::process::exit(1);
+        }
+    } else { Status::default() };
+
+    println!("{GREEN}{BOLD}{UNDERLINE}Strawberry Cloud{C_RESET}");
+    let percent = calc_percent(status.storage_quota_used, status.storage_quota_max);
+
+    let progress_bar = ProgressBar::new(status.storage_quota_max);
+
+    let template = String::from("{percent}% [{bar:40.cyan/blue}] {used}/{max} - {left} left")
+        .replace("{used}", &format_size(status.storage_quota_used as i64).to_string())
+        .replace("{max}", &format_size(status.storage_quota_max as i64).to_string())
+        .replace("{left}", &format_size(status.storage_quota_left).to_string());
+
+    progress_bar.set_style(
+        ProgressStyle::with_template(template.as_str())
+            .unwrap()
+            .progress_chars("=>-")
+    );
+
+    progress_bar.set_position(status.storage_quota_used);
+
+    if percent > 85.0 {
+        println!("{YELLOW}{BOLD}Warning: You used over 85% of your storage quota ({percent}%).{C_RESET}")
+    }
+    else {
+        println!();
+    }
 }
