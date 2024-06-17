381a5cd462a3743cfb0b27921451f621f1ec248c
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-17 21:11:10 +0200

    impl: added subcommand arg parsing

--- src/commands/backup.rs
+++ src/commands/backup.rs
@@ -1,3 +1,23 @@
+use crate::args::ARGS;
+use crate::commands;
+
 pub fn main() {
-    
+    match ARGS.subcommand.as_str() {
+        "restore" => {
+
+        }
+        "upload" => {
+
+        }
+        "add" => {
+
+        }
+        "remove" => {
+
+        }
+        "list" => {
+
+        }
+        _ => commands::help::help()
+    }
 }
