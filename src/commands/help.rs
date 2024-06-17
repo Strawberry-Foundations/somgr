cccdba0777857ee21036a54a526a9437501954d4
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-17 20:44:23 +0200

    impl: removed comment

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -28,13 +28,3 @@ pub fn help() {
 ", *VERSION);
     std::process::exit(0);
 }
-
-/*
-{BOLD}↳ {MAGENTA}Options:{C_RESET}
-            {CYAN}{BOLD}-u, --use <server>{C_RESET}      Select your target server for tunneling your traffic
-            {CYAN}{BOLD}-l, --local-host <host>{C_RESET} The address to expose                 {GREEN}{BOLD}[default: localhost]{C_RESET}
-            {CYAN}{BOLD}-p, --port <port>{C_RESET}       The port to expose                    {GREEN}{BOLD}[optional]{C_RESET}
-            {CYAN}{BOLD}-s, --secret <secret>{C_RESET}   Secret for authentication             {GREEN}{BOLD}[optional]{C_RESET}
-            {CYAN}{BOLD}-a, --auth{C_RESET}              Use Strawberry ID for Authentication  {GREEN}{BOLD}[optional]{C_RESET}
-            {CYAN}{BOLD}-cp, --control-port{C_RESET}     Control port for remote proxy server  {GREEN}{BOLD}[default: 7835]{C_RESET}
- */
