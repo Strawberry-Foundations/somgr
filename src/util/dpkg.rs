6ebbb6cf29870d1ddc5a33ec83bc07be10816a08
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-18 11:55:49 +0200

    fix: Fixed invalid chroot path for apt-lock

--- src/util/dpkg.rs
+++ src/util/dpkg.rs
@@ -3,7 +3,7 @@ use std::io::BufRead;
 
 pub fn get_packages() -> Vec<String> {
     subprocess::Exec::shell(
-        "/usr/sbin/chroot / dpkg --get-selections | grep -v deinstall | awk '{print $1}' > /var/cache/apt/package_list.txt"
+        "/usr/sbin/chroot /system dpkg --get-selections | grep -v deinstall | awk '{print $1}' > /var/cache/apt/package_list.txt"
     ).popen().unwrap();
 
     let file = File::open("/var/cache/apt/package_list.txt").unwrap();
