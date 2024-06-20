b7c9a31f1b26c0b67b95f23f4f2e98a094e00655
Juliandev02 <julian@strawberryfoundations.xyz>
2024-06-20 14:45:35 +0200

    impl: Added DPKG_SYSTEM_STATUS, DPKG_USER_STATUS & DPKG_USER_STATUS_TMP

--- src/statics.rs
+++ src/statics.rs
@@ -4,8 +4,12 @@ use stblib::logging::Logger;
 use stblib::colors::{BLUE, BOLD, C_RESET, CYAN, GREEN, RED, YELLOW};
 
 pub const STRAWBERRY_ID_API: &str = "https://id.strawberryfoundations.xyz/v2/";
-// pub const STRAWBERRY_CLOUD_API: &str = "http://localhost:8000/";
 pub const STRAWBERRY_CLOUD_API: &str = "https://cloud.strawberryfoundations.xyz/";
+// pub const STRAWBERRY_CLOUD_API: &str = "http://localhost:8000/";
+
+pub const DPKG_SYSTEM_STATUS: &str = "/system/var/lib/dpkg/status";
+pub const DPKG_USER_STATUS: &str = "/var/lib/dpkg/status";
+pub const DPKG_USER_STATUS_TMP: &str = "/tmp/user_status_updated";
 
 lazy_static! {
     pub static ref VERSION: String = env!("CARGO_PKG_VERSION").to_string();
