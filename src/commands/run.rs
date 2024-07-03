a44478b59473f4d94e6d8a065660b94c2887bf49
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-03 20:11:52 +0200

    feat: added run command

---
+++ src/commands/run.rs
@@ -0,0 +1,18 @@
+use crate::log_info;
+use crate::util::fs;
+use crate::util::verification::os_verifier;
+
+pub fn run() {
+    os_verifier();
+
+    log_info!("Mounting file systems ...");
+    fs::mount_system();
+
+    log_info!("Entering chroot ...");
+    subprocess::Exec::shell("sh -c '/usr/sbin/chroot /system'").popen().unwrap();
+
+    log_info!("Unmounting file systems ...");
+    fs::umount_system();
+
+    log_info!("Leaving chroot ...");
+}
