c630df3bb2f76f9eed8c3460be9a8a434197b8bf
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-20 13:46:02 +0200

    feat: add log_ok macro

--- src/util/log.rs
+++ src/util/log.rs
@@ -6,6 +6,13 @@ macro_rules! log_info {
 }
 
 #[macro_export]
+macro_rules! log_ok {
+    ($string:expr) => {
+        println!("\x1b[1m\x1b[44m    OK    \x1b[0m  {}\x1b[0m", $string);
+    };
+}
+
+#[macro_export]
 macro_rules! log_fail {
     ($string:expr) => {
         println!("\x1b[1m\x1b[41m   FAIL   \x1b[0m  {}\x1b[0m", $string);
