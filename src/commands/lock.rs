41e6124a3499547839a2be1eecb64144c9db1472
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-29 17:57:40 +0200

    feat: add verbose logging

--- src/commands/lock.rs
+++ src/commands/lock.rs
@@ -1,6 +1,7 @@
 use subprocess::NullFile;
 
 use crate::{log_fail, log_info, log_ok};
+use crate::args::{ARGS, OPTIONS};
 use crate::util::dpkg;
 use crate::util::verification::os_verifier;
 
@@ -11,12 +12,18 @@ pub fn lock() {
     log_info!("Locking apt ...");
 
     let packages = dpkg::get_packages();
-
-    let exit_status = subprocess::Exec::shell(format!("apt-mark hold {} &> /dev/null", packages.join(" ")))
-        .stdout(NullFile)
-        .stderr(NullFile)
-        .join()
-        .expect("Failed to execute command");
+    
+    let exit_status = if OPTIONS.verbose {
+        subprocess::Exec::shell(format!("apt-mark hold {}", packages.join(" ")))
+            .join()
+            .expect("Failed to execute command")
+    } else {
+        subprocess::Exec::shell(format!("apt-mark hold {} &> /dev/null", packages.join(" ")))
+            .stdout(NullFile)
+            .stderr(NullFile)
+            .join()
+            .expect("Failed to execute command")
+    }; 
 
     if exit_status.success() {
         log_ok!("Finished apt locking");
