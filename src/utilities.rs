5b075b3fdec949a427eb098750fe8a5e70247ec6
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-31 21:14:06 +0200

    feat: remove utilities.rs

--- src/utilities.rs
+++
@@ -1,9 +0,0 @@
-use std::path::{Path, PathBuf};
-
-
-
-pub fn serializer(text: &str) -> Result<serde_json::Value, serde_json::Error> {
-    let serializer = serde_json::from_str(text)?;
-    Ok(serializer)
-}
-
