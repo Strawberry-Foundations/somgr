43bb33ca738d7af53bed562f808f5b48c141d7aa
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-31 21:00:19 +0200

    feat: add popen function

--- src/core/subprocess.rs
+++ src/core/subprocess.rs
@@ -1,4 +1,4 @@
-use subprocess::NullFile;
+use subprocess::{NullFile, Popen};
 
 use crate::args::OPTIONS;
 use crate::log_panic;
@@ -22,6 +22,12 @@ pub fn subprocess(cmd: impl ToString) -> subprocess::ExitStatus {
                 log_panic!("Failed to execute command");
             })
     }
+}
+
+pub fn popen(cmd: impl ToString) -> Popen {
+    let command = cmd.to_string();
+
+    subprocess::Exec::shell(command).popen().unwrap()
 }
 
 #[macro_export]
