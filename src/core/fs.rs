d317e539ea923176874e2d32775ce3a1c7d25437
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-31 22:14:03 +0200

    chore: remove not working function

--- src/core/fs.rs
+++ src/core/fs.rs
@@ -1,8 +1,3 @@
-use std::fs::File;
-use std::os::fd::AsRawFd;
-use nix::fcntl::{PosixFadviseAdvice, posix_fadvise};
-use nix::unistd::{Whence, lseek};
-
 pub fn mount_system() {
     subprocess::Exec::shell("sh -c 'mount --bind /dev /system/dev'").popen().unwrap();
     subprocess::Exec::shell("sh -c 'mount --bind /sys /system/sys'").popen().unwrap();
@@ -13,11 +8,4 @@ pub fn umount_system() {
     subprocess::Exec::shell("sh -c 'umount /system/dev'").popen().unwrap();
     subprocess::Exec::shell("sh -c 'umount /system/sys'").popen().unwrap();
     subprocess::Exec::shell("sh -c 'umount /system/proc'").popen().unwrap();
-}
-
-pub fn fadvise(path: impl ToString) {
-    let file = File::open(path.to_string()).unwrap();
-    let file_len = lseek(file.as_raw_fd(), 0, Whence::SeekEnd).unwrap();
-
-    posix_fadvise(file.as_raw_fd(), 0, file_len, PosixFadviseAdvice::POSIX_FADV_DONTNEED).unwrap();
 }
