8f74c7bb62c633fa407494c4d2139d563a75caae
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-31 21:11:54 +0200

    feat: move several functions from utilities.rs to util/*

--- src/util/fmt.rs
+++ src/util/fmt.rs
@@ -16,3 +16,24 @@ pub fn format_size(kilobytes: i64) -> String {
         format!("{:.1} MB", bytes as f64 / MB as f64)
     }
 }
+
+pub fn calc_percent(part: u64, val: u64) -> f64 {
+    let result = if val != 0 {
+        (part as f64 / val as f64) * 100.0
+    } else {
+        0.0
+    };
+
+    result.ceil()
+}
+
+pub fn capitalize_first(input: &str) -> String {
+    if input.is_empty() {
+        String::new()
+    } else {
+        let mut chars = input.chars();
+        let first_char = chars.next().unwrap().to_uppercase();
+        let rest_of_string: String = chars.collect();
+        format!("{}{}", first_char, rest_of_string)
+    }
+}
