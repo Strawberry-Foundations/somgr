4aea8da4acc0e8c490e61c7fc2ea61086c39bc31
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-31 22:14:04 +0200

    chore: remove not working function

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -7,10 +7,9 @@ use crate::commands::lock::lock;
 use crate::commands::mount::{mount, umount, remount, MountType};
 use crate::util::verification::os_verifier;
 use crate::core::dpkg::{get_package_version, update_version_in_entry};
-use crate::statics::{DPKG_SYSTEM_STATUS, DPKG_USER_STATUS, DPKG_USER_STATUS_TMP};
 use crate::core::args::OPTIONS;
+use crate::statics::{DPKG_SYSTEM_STATUS, DPKG_USER_STATUS, DPKG_USER_STATUS_TMP};
 use crate::{log_info, log_ok, log_warn};
-use crate::core::fs::fadvise;
 
 pub fn update() {
     os_verifier();
@@ -113,9 +112,7 @@ pub fn resolve_status_file_conflict() -> eyre::Result<()> {
     }
 
     fs::rename(DPKG_USER_STATUS_TMP, DPKG_USER_STATUS).unwrap();
-
-    fadvise("/var/lib/dpkg/status");
-
+    
     log_info!("Package status update completed");
     Ok(())
 }
