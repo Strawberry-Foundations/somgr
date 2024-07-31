4606eccb3e238ef0408da7bc6600a3e8df2fb276
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-31 20:58:38 +0200

    feat: improve command execution

--- src/commands/unlock.rs
+++ src/commands/unlock.rs
@@ -1,26 +1,21 @@
-use subprocess::NullFile;
-
-use crate::{log_fail, log_info, log_ok};
+use crate::{log_info, log_ok, log_panic};
+use crate::core::subprocess::subprocess;
 use crate::util::dpkg;
 use crate::util::verification::os_verifier;
 
 pub fn unlock() {
     os_verifier();
     karen::escalate_if_needed().unwrap();
-    
+
     log_info!("Unlocking apt (DANGEROUS!) ...");
 
     let packages = dpkg::get_packages();
 
-    let exit_status = subprocess::Exec::shell(format!("apt-mark unhold {} &> /dev/null", packages.join(" ")))
-        .stdout(NullFile)
-        .stderr(NullFile)
-        .join()
-        .expect("Failed to execute command");
+    let exit_status = subprocess(format!("apt-mark unhold {}", packages.join(" ")));
 
     if exit_status.success() {
         log_ok!("Finished apt unlocking");
     } else {
-        log_fail!("Command execution failed");
+        log_panic!("Command execution failed");
     }
 }
