d71fb11885957951a1d2544c3fdd58896862ead9
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-31 21:11:57 +0200

    feat: move several functions from utilities.rs to util/*

--- src/utilities.rs
+++ src/utilities.rs
@@ -7,36 +7,3 @@ pub fn serializer(text: &str) -> Result<serde_json::Value, serde_json::Error> {
     Ok(serializer)
 }
 
-pub fn capitalize_first(input: &str) -> String {
-    if input.is_empty() {
-        String::new()
-    } else {
-        let mut chars = input.chars();
-        let first_char = chars.next().unwrap().to_uppercase();
-        let rest_of_string: String = chars.collect();
-        format!("{}{}", first_char, rest_of_string)
-    }
-}
-
-pub fn calc_percent(part: u64, val: u64) -> f64 {
-    let result = if val != 0 {
-        (part as f64 / val as f64) * 100.0
-    } else {
-        0.0
-    };
-
-    result.ceil()
-}
-
-pub fn make_absolute_path(input_path: &str) -> PathBuf {
-    let path = Path::new(input_path);
-
-    if path.is_absolute() {
-        PathBuf::from(path)
-    } else {
-        // Wenn der Pfad relativ ist, füge das aktuelle Arbeitsverzeichnis hinzu
-        let mut absolute_path = std::env::current_dir().expect("Konnte das aktuelle Verzeichnis nicht abrufen.");
-        absolute_path.push(path);
-        absolute_path
-    }
-}
