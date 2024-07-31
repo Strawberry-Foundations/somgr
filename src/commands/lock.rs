bcd83ea6bf34c5b01327d07166cccf6e3b7f6d26
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-31 20:57:09 +0200

    feat: improve command execution

--- src/commands/lock.rs
+++ src/commands/lock.rs
@@ -1,7 +1,5 @@
-use subprocess::NullFile;
-
-use crate::{log_fail, log_info, log_ok};
-use crate::args::OPTIONS;
+use crate::{log_info, log_ok, log_panic};
+use crate::core::subprocess::subprocess;
 use crate::util::dpkg;
 use crate::util::verification::os_verifier;
 
@@ -13,21 +11,11 @@ pub fn lock() {
 
     let packages = dpkg::get_packages();
 
-    let exit_status = if OPTIONS.verbose {
-        subprocess::Exec::shell(format!("apt-mark hold {}", packages.join(" ")))
-            .join()
-            .expect("Failed to execute command")
-    } else {
-        subprocess::Exec::shell(format!("apt-mark hold {} &> /dev/null", packages.join(" ")))
-            .stdout(NullFile)
-            .stderr(NullFile)
-            .join()
-            .expect("Failed to execute command")
-    };
+    let exit_status = subprocess(format!("apt-mark hold {}", packages.join(" ")));
 
     if exit_status.success() {
         log_ok!("Finished apt locking");
     } else {
-        log_fail!("Command execution failed");
+        log_panic!("Command execution failed");
     }
 }
