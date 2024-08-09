f055210a583e8bf7999a5cc81ff0546a3f284142
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-09 14:18:08 +0200

    fix: fix update cmd

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -1,12 +1,12 @@
 use std::collections::HashSet;
 use std::fs::{self, File};
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{BufRead, BufReader, Write};
 use eyre::eyre;
 
 use crate::commands::lock::lock;
 use crate::commands::mount::{mount, umount, remount, MountType};
 use crate::util::verification::os_verifier;
-use crate::core::dpkg::status::{get_package_version, update_version_in_entry, get_package_entry};
+use crate::core::dpkg::status::{get_package_version, get_package_entry};
 use crate::core::args::OPTIONS;
 use crate::core::fs::drop_fs_cache;
 use crate::statics::{DPKG_SYSTEM_STATUS, DPKG_USER_STATUS, DPKG_USER_STATUS_TMP};
@@ -62,30 +62,6 @@ pub fn resolve_status_file_conflict() -> eyre::Result<()> {
     let mut user_packages = DpkgStatus::from_status_file("/var/lib/dpkg/status");
     let system_packages = DpkgStatus::from_status_file(DPKG_SYSTEM_STATUS);
 
-    // Get all installed packages in the user status file
-    let _user_packages: HashSet<String> = {
-        let file = File::open(DPKG_USER_STATUS).unwrap();
-        let reader = BufReader::new(file);
-        reader.lines()
-            .map_while(Result::ok)
-            .filter(|line| line.starts_with("Package: "))
-            .map(|line| line.split_whitespace().nth(1).unwrap().to_string())
-            .collect()
-    };
-
-    // Get all installed packages in the system status file
-    let _system_packages: HashSet<String> = {
-        let file = File::open(DPKG_SYSTEM_STATUS).unwrap();
-        let reader = BufReader::new(file);
-        reader.lines()
-            .map_while(Result::ok)
-            .filter(|line| line.starts_with("Package: "))
-            .map(|line| line.split_whitespace().nth(1).unwrap().to_string())
-            .collect()
-    };
-
-    let mut package_entry = String::new();
-    let mut user_statusfile = BufReader::new(File::open(DPKG_USER_STATUS).unwrap());
     let mut user_statusfile_tmp = File::create(DPKG_USER_STATUS_TMP).unwrap();
 
     for package in user_packages.clone().packages {
@@ -113,48 +89,30 @@ pub fn resolve_status_file_conflict() -> eyre::Result<()> {
             log_warn!(format!("Skipping package {}: Not system-wide installed", package.package.as_ref().unwrap()));
         }
     }
-    
-    /* for line in user_statusfile.by_ref().lines() {
-        let line = line.unwrap();
-        if line.trim().is_empty() {
-            if let Some(package_name) = package_entry.lines().find_map(|line| {
-                if line.starts_with("Package: ") {
-                    line.split_whitespace().nth(1).map(String::from)
-                } else {
-                    None
-                }
-            }) {
-                if _user_packages.contains(&package_name) {
-                    let system_version = get_package_version(&package_name, DPKG_SYSTEM_STATUS);
-                    let user_version = get_package_version(&package_name, DPKG_USER_STATUS);
 
-                    if let (Some(system_version), Some(user_version)) = (system_version, user_version) {
-                        if system_version != user_version {
-                            log_info!(format!("Update {} from version {} to {}", package_name, user_version, system_version));
-                            let updated_entry = update_version_in_entry(&package_entry, &system_version);
-                            writeln!(user_statusfile_tmp, "{}\n", updated_entry).unwrap();
-                        } else {
-                            writeln!(user_statusfile_tmp, "{}\n", package_entry).unwrap();
-                        }
-                    } else {
-                        writeln!(user_statusfile_tmp, "{}\n", package_entry).unwrap();
-                    }
-                } else {
-                    panic!("SYSTEM HALT");
-                    writeln!(user_statusfile_tmp, "{}\n", package_entry).unwrap();
-                }
-            }
-            package_entry.clear();
-        } else {
-            package_entry.push_str(&line);
-            package_entry.push('\n');
-        }
-    } */
+    // Get all installed packages in the user status file
+    let _user_packages: HashSet<String> = {
+        let file = File::open(DPKG_USER_STATUS).unwrap();
+        let reader = BufReader::new(file);
+        reader.lines()
+            .map_while(Result::ok)
+            .filter(|line| line.starts_with("Package: "))
+            .map(|line| line.split_whitespace().nth(1).unwrap().to_string())
+            .collect()
+    };
 
-    // Handle the last package entry if not empty
-    if !package_entry.is_empty() {
-        writeln!(user_statusfile_tmp, "{}\n", package_entry).unwrap();
-    }
+    // Get all installed packages in the system status file
+    let _system_packages: HashSet<String> = {
+        let file = File::open(DPKG_SYSTEM_STATUS).unwrap();
+        let reader = BufReader::new(file);
+        reader.lines()
+            .map_while(Result::ok)
+            .filter(|line| line.starts_with("Package: "))
+            .map(|line| line.split_whitespace().nth(1).unwrap().to_string())
+            .collect()
+    };
+
+    log_info!("Checking for new packages");
 
     // Add missing packages from the system status file to the user status file
     for package_name in _system_packages.difference(&_user_packages) {
@@ -166,8 +124,6 @@ pub fn resolve_status_file_conflict() -> eyre::Result<()> {
             writeln!(user_statusfile_tmp, "{}\n", entry).unwrap();
         }
     }
-
-    // fs::rename(DPKG_USER_STATUS_TMP, DPKG_USER_STATUS).unwrap();
 
     log_info!("Package status update completed");
     Ok(())
